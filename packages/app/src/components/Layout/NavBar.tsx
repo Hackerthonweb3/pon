2423d774159c84fcee19c036ee78aee0bc5edc38
Otto G <5030059+ottodevs@users.noreply.github.com>
2022-09-06 01:57:52 +0200

    fix: error rendering navbar

--- packages/app/src/components/Layout/NavBar.tsx
+++ packages/app/src/components/Layout/NavBar.tsx
@@ -1,12 +1,11 @@
 import Image from 'next/image'
 import NextLink from 'next/link'
 import { useRouter } from 'next/router'
-import { Box, HStack, Flex, useColorModeValue } from '@chakra-ui/react'
+import { Box, HStack, Flex, Text, useColorModeValue } from '@chakra-ui/react'
 import { useAccount, useDisconnect } from 'wagmi'
 import { ToggleColorMode } from './ToggleColorMode'
 import { NavLink } from './NavLink'
 import { NavMobile } from './NavMobile'
-import LogoSvg from '~/media/logo.svg'
 import { useOrbis } from '~/hooks'
 
 const links = [
@@ -49,7 +48,9 @@ export const NavBar = () => {
                 <HStack h={{ md: 20, base: 16 }} alignItems='center' justifyContent={'space-between'}>
                     <Box style={{ fontWeight: '800', cursor: 'pointer' }}>
                         <NextLink passHref href='/'>
-                            <Image height='50px' width='60px' src={LogoSvg} alt='logo' />
+                            <Text color={'gray.400'} fontSize='30px'>
+                                Web3 Digital Business Card
+                            </Text>
                         </NextLink>
                     </Box>
                     <HStack display={displayNonMobile} spacing={{ md: 6 }}>
