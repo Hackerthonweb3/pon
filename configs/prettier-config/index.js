223b3e1e8803e7a6bf244f53868d04b71d7073fc
Otto G <5030059+ottodevs@users.noreply.github.com>
2022-09-05 18:02:30 +0200

    chore: prettier and eslint configs

---
+++ configs/prettier-config/index.js
@@ -0,0 +1,21 @@
+module.exports = {
+    arrowParens: 'avoid',
+    printWidth: 120,
+    endOfLine: 'lf',
+    bracketSpacing: true,
+    bracketSameLine: true,
+    jsxSingleQuote: true,
+    tabWidth: 4,
+    useTabs: false,
+    quoteProps: 'consistent',
+    semi: false,
+    singleQuote: true,
+    trailingComma: 'all',
+    proseWrap: 'preserve',
+    plugins: ['./node_modules/prettier-plugin-solidity', './node_modules/prettier-plugin-jsdoc'],
+    overrides: [
+        { files: '*.json', options: { parser: 'json' } },
+        { files: '*.yaml', options: { parser: 'yaml' } },
+        { files: '*.sol', options: { parser: 'solidity-parse' } },
+    ],
+}
