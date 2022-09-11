58f2234d21a0ce0d24a254046698f2aa2c9fe53b
Otto G <5030059+ottodevs@users.noreply.github.com>
2022-09-11 23:59:26 +0200

    chore: move app package to apps/web for consistency

---
+++ apps/web/next.config.mjs
@@ -0,0 +1,16 @@
+// @ts-check
+
+import transpile from 'next-transpile-modules'
+
+const withTM = transpile(['@business-card/sdk', '@business-card/sol'])
+
+/** @type {import('next').NextConfig} */
+const nextConfig = withTM({
+    compiler: { removeConsole: true },
+    images: { domains: ['business-card.infura-ipfs.io'] },
+    poweredByHeader: false,
+    reactStrictMode: true,
+    swcMinify: true,
+})
+
+export default nextConfig
