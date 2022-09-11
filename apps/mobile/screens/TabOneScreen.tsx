234d16daca0f50a27f1c6468171e7190046e6958
Otto G <5030059+ottodevs@users.noreply.github.com>
2022-09-11 23:51:27 +0200

    feat: create expo app boilerplate
    NOTES: Because react-native does not support symlinks, it was needed to change pnpm config to hoist the deps in the monorepo root, among some metro config customizations

---
+++ apps/mobile/screens/TabOneScreen.tsx
@@ -0,0 +1,35 @@
+import { StyleSheet } from 'react-native'
+
+import EditScreenInfo from '../components/EditScreenInfo'
+import { Text, View } from '../components/Themed'
+import { RootTabScreenProps } from '../types'
+import { useSdk } from '@business-card/sdk'
+
+export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+    const currentText = useSdk()
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>Tab One</Text>
+            <Text>{currentText}</Text>
+            <View style={styles.separator} lightColor='#eee' darkColor='rgba(255,255,255,0.1)' />
+            <EditScreenInfo path='/screens/TabOneScreen.tsx' />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: 'bold',
+    },
+    separator: {
+        marginVertical: 30,
+        height: 1,
+        width: '80%',
+    },
+})
