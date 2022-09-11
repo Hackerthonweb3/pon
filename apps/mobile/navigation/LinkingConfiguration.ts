f6ff9b592f1a619e3bf6a9cbad3eaefc95c745e7
Otto G <5030059+ottodevs@users.noreply.github.com>
2022-09-11 23:55:28 +0200

    chore: lint mobile app

--- apps/mobile/navigation/LinkingConfiguration.ts
+++ apps/mobile/navigation/LinkingConfiguration.ts
@@ -1,36 +1,35 @@
 /**
- * Learn more about deep linking with React Navigation
- * https://reactnavigation.org/docs/deep-linking
+ * Learn more about deep linking with React Navigation https://reactnavigation.org/docs/deep-linking
  * https://reactnavigation.org/docs/configuring-links
  */
 
-import { LinkingOptions } from '@react-navigation/native';
-import * as Linking from 'expo-linking';
+import { LinkingOptions } from '@react-navigation/native'
+import * as Linking from 'expo-linking'
 
-import { RootStackParamList } from '../types';
+import { RootStackParamList } from '../types'
 
 const linking: LinkingOptions<RootStackParamList> = {
-  prefixes: [Linking.makeUrl('/')],
-  config: {
-    screens: {
-      Root: {
+    prefixes: [Linking.makeUrl('/')],
+    config: {
         screens: {
-          TabOne: {
-            screens: {
-              TabOneScreen: 'one',
+            Root: {
+                screens: {
+                    TabOne: {
+                        screens: {
+                            TabOneScreen: 'one',
+                        },
+                    },
+                    TabTwo: {
+                        screens: {
+                            TabTwoScreen: 'two',
+                        },
+                    },
+                },
             },
-          },
-          TabTwo: {
-            screens: {
-              TabTwoScreen: 'two',
-            },
-          },
+            Modal: 'modal',
+            NotFound: '*',
         },
-      },
-      Modal: 'modal',
-      NotFound: '*',
     },
-  },
-};
+}
 
-export default linking;
+export default linking
