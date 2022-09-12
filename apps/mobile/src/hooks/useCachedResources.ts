c9e6ebdb00b6d40ac3d6ce786bbaca17bfef6fc3
Otto G <5030059+ottodevs@users.noreply.github.com>
2022-09-12 10:34:10 +0200

    feat: replace default font by inter

--- apps/mobile/src/hooks/useCachedResources.ts
+++ apps/mobile/src/hooks/useCachedResources.ts
@@ -1,4 +1,4 @@
-import { FontAwesome } from '@expo/vector-icons'
+import { MaterialCommunityIcons } from '@expo/vector-icons'
 import * as Font from 'expo-font'
 import * as SplashScreen from 'expo-splash-screen'
 import { useEffect, useState } from 'react'
@@ -14,8 +14,8 @@ export default function useCachedResources() {
 
                 // Load fonts
                 await Font.loadAsync({
-                    ...FontAwesome.font,
-                    'space-mono': require('../assets/fonts/SpaceMono-Regular.ttf'),
+                    ...MaterialCommunityIcons.font,
+                    Inter: require('../assets/fonts/Inter-Regular.otf'),
                 })
             } catch (e) {
                 // We might want to provide this error information to an error reporting service
