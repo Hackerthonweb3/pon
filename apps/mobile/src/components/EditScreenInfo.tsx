07f78d8552f6b84114e04cc0279a39bedab600b5
cNoveron <carlos.escom.ipn@gmail.com>
2022-09-12 21:33:38 -0500

    + QRCode screen v0

--- apps/mobile/src/components/EditScreenInfo.tsx
+++ apps/mobile/src/components/EditScreenInfo.tsx
@@ -9,28 +9,12 @@ export default function EditScreenInfo({ path }: { path: string }) {
     return (
         <View>
             <View style={styles.getStartedContainer}>
+                <Text style={styles.title}>How to get a Proof of Networking</Text>
                 <Text style={styles.getStartedText} lightColor='rgba(0,0,0,0.8)' darkColor='rgba(255,255,255,0.8)'>
-                    Open up the code for this screen:
-                </Text>
-
-                <View
-                    style={[styles.codeHighlightContainer, styles.homeScreenFilename]}
-                    darkColor='rgba(255,255,255,0.05)'
-                    lightColor='rgba(0,0,0,0.05)'>
-                    <MonoText>{path}</MonoText>
-                </View>
-
-                <Text style={styles.getStartedText} lightColor='rgba(0,0,0,0.8)' darkColor='rgba(255,255,255,0.8)'>
+                    Scan the QR code in your new acquaintance's mobile app:{' '}
+                    <MonoText style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>{path}</MonoText>{' '}
                     Change any of the text, save the file, and your app will automatically update.
                 </Text>
-            </View>
-
-            <View style={styles.helpContainer}>
-                <TouchableOpacity onPress={handleHelpPress} style={styles.helpLink}>
-                    <Text style={styles.helpLinkText} lightColor={Colors.light.tint}>
-                        Tap here if your app doesn't automatically update after making changes
-                    </Text>
-                </TouchableOpacity>
             </View>
         </View>
     )
@@ -45,7 +29,7 @@ function handleHelpPress() {
 const styles = StyleSheet.create({
     getStartedContainer: {
         alignItems: 'center',
-        marginHorizontal: 50,
+        marginHorizontal: 30,
     },
     homeScreenFilename: {
         marginVertical: 7,
@@ -55,19 +39,13 @@ const styles = StyleSheet.create({
         paddingHorizontal: 4,
     },
     getStartedText: {
-        fontSize: 17,
-        lineHeight: 24,
+        fontSize: 13,
+        lineHeight: 20,
         textAlign: 'center',
     },
-    helpContainer: {
-        marginTop: 15,
-        marginHorizontal: 20,
-        alignItems: 'center',
-    },
-    helpLink: {
-        paddingVertical: 15,
-    },
-    helpLinkText: {
-        textAlign: 'center',
+    title: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        marginBottom: 10,
     },
 })
