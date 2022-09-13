454eeb4d4a089738a0fa76286df2be897c4b60ad
cNoveron <carlos.escom.ipn@gmail.com>
2022-09-12 19:35:40 -0500

    + QR scanner code

--- apps/mobile/src/screens/QRCode.tsx
+++ apps/mobile/src/screens/QRCode.tsx
@@ -1,5 +1,11 @@
-import { StyleSheet } from 'react-native'
-
+import {
+    AppRegistry,
+    StyleSheet,
+    // Text,
+    TouchableOpacity,
+    Linking
+} from 'react-native';
+  
 import EditScreenInfo from '../components/EditScreenInfo'
 import { Text, View } from '../components/Themed'
 import { RootTabScreenProps } from '../types'
@@ -8,6 +14,12 @@ import QRCodeScanner from 'react-native-qrcode-scanner';
 import { RNCamera } from 'react-native-camera';
 
 export default function QRCode({ navigation }: RootTabScreenProps<'QRCode'>) {
+    const onSuccess = e => {
+      Linking.openURL(e.data).catch(err =>
+        console.error('An error occured', err)
+      );
+    };
+
     const currentText = useSdk()
     return (
         <View style={styles.container}>
@@ -15,6 +27,22 @@ export default function QRCode({ navigation }: RootTabScreenProps<'QRCode'>) {
             <Text>{currentText}</Text>
             <View style={styles.separator} lightColor='#eee' darkColor='rgba(255,255,255,0.1)' />
             <EditScreenInfo path='/screens/QRCode.tsx' />
+            <QRCodeScanner
+                onRead={onSuccess}
+                flashMode={RNCamera.Constants.FlashMode.torch}
+                topContent={
+                <Text style={styles.centerText}>
+                    Go to{' '}
+                    <Text style={styles.textBold}>wikipedia.org/wiki/QR_code</Text> on
+                    your computer and scan the QR code.
+                </Text>
+                }
+                bottomContent={
+          <TouchableOpacity style={styles.buttonTouchable}>
+            <Text style={styles.buttonText}>OK. Got it!</Text>
+          </TouchableOpacity>
+        }
+      />
         </View>
     )
 }
@@ -34,4 +62,21 @@ const styles = StyleSheet.create({
         height: 1,
         width: '80%',
     },
+    centerText: {
+      flex: 1,
+      fontSize: 18,
+      padding: 32,
+      color: '#777'
+    },
+    textBold: {
+      fontWeight: '500',
+      color: '#000'
+    },
+    buttonText: {
+      fontSize: 21,
+      color: 'rgb(0,122,255)'
+    },
+    buttonTouchable: {
+      padding: 16
+    }
 })
