fbd704c75880ffd8b85e273ee6748f20883f67f8
Otto G <5030059+ottodevs@users.noreply.github.com>
2022-09-19 16:03:47 +0200

    feat: separate scan/show code components

--- apps/mobile/src/screens/QRCode.tsx
+++ apps/mobile/src/screens/QRCode.tsx
@@ -1,22 +1,58 @@
-import React, { useState, useEffect } from 'react'
-import {
-    ActivityIndicator,
-    AppRegistry,
-    Button,
-    StyleSheet,
-    // Text,
-    TouchableOpacity,
-    Linking,
-    Image,
-    Modal,
-} from 'react-native'
-import EditScreenInfo from '../components/EditScreenInfo'
 import { Text, View } from '../components/Themed'
-import { RootTabScreenProps, Setter } from '../types'
-import { useSdk } from '@business-card/sdk'
+import { CodeScan } from '../components/CodeScan'
+import { Dimensions, StyleSheet, TouchableOpacity } from 'react-native'
+import { BottomTabBarProps, createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import type { BottomTabNavigationOptions } from '@react-navigation/bottom-tabs'
+import QRIcon from '../assets/images/qr.svg'
+import ScanIcon from '../assets/images/scan.svg'
+import { useEffect, useState } from 'react'
+import { Setter } from '../types'
 import { BarCodeScanner } from 'expo-barcode-scanner'
 
-export default function QRCode({ navigation }: RootTabScreenProps<'QRCode'>) {
+const BottomTab = createBottomTabNavigator()
+
+interface TabOptions {
+    color: string
+    onPress: () => void
+    icon: (props: { color: string }) => React.ReactNode
+    tab: {
+        name: string
+    }
+}
+
+const Tab = ({ color, tab, onPress, icon }: TabOptions) => {
+    return (
+        <TouchableOpacity style={styles.tabContainer}>
+            {icon({ color })}
+            <Text style={{ ...styles.tabText, color }}>{tab.name}</Text>
+        </TouchableOpacity>
+    )
+}
+
+const { width } = Dimensions.get('screen')
+
+const TabBar = ({ state, descriptors }: BottomTabBarProps) => {
+    const { routes } = state
+
+    return (
+        <View style={styles.wrapper}>
+            <View style={styles.container}>
+                {routes.map(route => (
+                    <Tab
+                        tab={route}
+                        icon={descriptors[route.key].options.tabBarIcon as TabOptions['icon']}
+                        onPress={() => {}}
+                        color='#fff'
+                        key={route.key}
+                    />
+                ))}
+            </View>
+            <View style={styles.separator} />
+        </View>
+    )
+}
+
+export default function QRCode() {
     const [hasPermission, setHasPermission] = useState(false)
     const [scannerOn, setScannerOn] = useState(false)
     const [activityIndicatorIsVisible, setActivityIndicatorIsVisible] = useState(false)
@@ -50,77 +86,27 @@ export default function QRCode({ navigation }: RootTabScreenProps<'QRCode'>) {
         getBarCodeScannerPermissions()
     }, [])
 
-    if (hasPermission === null) {
-        return <Text>Requesting for camera permission</Text>
-    }
-    if (hasPermission === false) {
-        return <Text>No access to camera</Text>
-    }
+    // if (hasPermission === null) {
+    //     return <Text>Requesting for camera permission</Text>
+    // }
+    // if (hasPermission === false) {
+    //     return <Text>No access to camera</Text>
+    // }
 
     return (
         <View style={styles.screen}>
-            <View style={styles.headerContainer}>
-                <Text style={styles.header} category='h4'>
-                    Get Proof of Networking
-                </Text>
-            </View>
-
-            <View style={styles.container}>
-                <View style={styles.textContainer}>
-                    <Text style={styles.title}>How to get a Proof of Networking</Text>
-                    <Text style={styles.text} lightColor='rgba(0,0,0,0.8)' darkColor='rgba(255,255,255,0.8)'>
-                        Scan the QR code in your new acquaintance's mobile app:{' '}
-                    </Text>
-                </View>
-                <BarCodeScanner
-                    barCodeTypes={[BarCodeScanner.Constants.BarCodeType.qr]}
-                    onBarCodeScanned={
-                        scannerOn && !messageModalIsVisible && !activityIndicatorIsVisible
-                            ? handleBarCodeScanned
-                            : undefined
-                    }
-                    style={styles.barCodeScanner}
+            <BottomTab.Navigator initialRouteName='QRcode' tabBar={props => <TabBar {...props} />}>
+                <BottomTab.Screen
+                    name='QRcode'
+                    component={CodeScan}
+                    options={{ tabBarIcon: ({ color }) => <QRIcon color={color} width={38} height={38} /> }}
                 />
-            </View>
-
-            <Modal style={{ backgroundColor: 'transparent' }} transparent={true} visible={activityIndicatorIsVisible}>
-                <ActivityIndicator
-                    animating={activityIndicatorIsVisible}
-                    color='#00ff00'
-                    size={100}
-                    style={{ flex: 1 }}
+                <BottomTab.Screen
+                    name='Scan'
+                    component={CodeScan}
+                    options={{ tabBarIcon: ({ color }) => <ScanIcon color={color} width={38} height={38} /> }}
                 />
-            </Modal>
-
-            <Modal
-                style={{ backgroundColor: 'transparent' }}
-                onDismiss={hideMessageModal}
-                transparent={true}
-                visible={messageModalIsVisible}>
-                <View style={styles.modalView}>
-                    <View style={styles.modalViewView}>
-                        <Text style={styles.modalText}>You have connected with Hidetaka!</Text>
-                        <View style={styles.avatarView}>
-                            <Image style={styles.avatar} source={require('../assets/images/hidetaka.png')} />
-                        </View>
-                        <Button title={'Done'} onPress={hideMessageModal} />
-                    </View>
-                </View>
-            </Modal>
-
-            <Modal transparent={true} visible={!scannerOn}>
-                <View style={styles.controlsModalView}>
-                    <View style={styles.mainControls}>
-                        <TouchableOpacity style={styles.controlBox} onPress={() => setScannerOn(true)}>
-                            <Text>Scan</Text>
-                        </TouchableOpacity>
-                        <View style={{ height: '100%', width: 2, backgroundColor: '#555' }} />
-                        <TouchableOpacity style={styles.controlBox} onPress={() => setScannerOn(false)}>
-                            <Text>Show QR</Text>
-                        </TouchableOpacity>
-                    </View>
-                </View>
-            </Modal>
+            </BottomTab.Navigator>
         </View>
     )
 }
@@ -128,114 +114,42 @@ export default function QRCode({ navigation }: RootTabScreenProps<'QRCode'>) {
 const styles = StyleSheet.create({
     screen: {
         flex: 1,
-        alignItems: 'stretch',
-        justifyContent: 'space-between',
+        backgroundColor: 'transparent',
     },
-    textBold: {
-        fontWeight: '500',
-        color: '#000',
-    },
-    buttonText: {
-        fontSize: 21,
-        color: 'rgb(0,122,255)',
-    },
-    button: {
-        padding: 16,
-    },
-    barCodeScanner: {
-        flex: 1,
-        alignSelf: 'center',
-        width: '100%',
-    },
-    headerContainer: {
-        height: 100,
+    wrapper: {
+        position: 'absolute',
+        bottom: 38,
+        width,
         alignItems: 'center',
         justifyContent: 'center',
-        backgroundColor: '#251819',
-    },
-    header: {
-        color: '#fff',
-        fontWeight: '600',
-        fontSize: 32,
-        marginTop: 40,
-        paddingHorizontal: 20,
-        paddingVertical: 10,
+        backgroundColor: 'transparent',
+        paddingLeft: 20,
+        paddingRight: 20,
     },
     container: {
+        flexDirection: 'row',
+        height: 100,
+        backgroundColor: 'rgba(0, 0, 0, 0.28)',
+        // backgroundColor: '#333',
+        justifyContent: 'space-between',
+        borderRadius: 14,
+    },
+    tabContainer: {
         flex: 1,
         alignItems: 'center',
         justifyContent: 'flex-start',
         padding: 20,
     },
-    textContainer: {
-        alignItems: 'flex-start',
-        textAlign: 'justify',
-        marginBottom: 20,
-        height: 100,
-        width: '100%',
+    separator: {
+        position: 'absolute',
+        width: 1,
+        height: 60,
+        backgroundColor: 'rgba(255, 255, 255, 0.12)',
     },
-    title: {
-        flex: 1,
-        fontSize: 20,
-        fontWeight: 'bold',
-        marginBottom: 10,
-    },
-    text: {
-        flex: 1,
-        fontSize: 18,
-        lineHeight: 20,
-        alignItems: 'center',
-        color: '#ccc',
-    },
-    modalText: {
-        color: 'white',
-        alignSelf: 'center',
-        fontSize: 14,
-        margin: 10,
-    },
-    modalView: {
-        flex: 1,
-        flexDirection: 'column',
-        justifyContent: 'space-around',
-        height: '100%',
-        backgroundColor: 'rgba(0,0,0,0)',
-    },
-    modalViewView: {
-        alignSelf: 'center',
-        width: 300,
-        margin: 10,
-        padding: 10,
-        borderRadius: 20,
-        backgroundColor: 'rgba(0,0,0,0.66)',
-    },
-    avatarView: {
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: 'transparent',
-    },
-    avatar: {
-        margin: 10,
-    },
-    controlsModalView: {
-        flexDirection: 'column',
-        justifyContent: 'flex-end',
-        height: '100%',
-        backgroundColor: 'rgba(0,0,0,0)',
-    },
-    mainControls: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        alignSelf: 'center',
-        justifyContent: 'space-around',
-        height: 120,
-        marginHorizontal: 30,
-        marginVertical: 140,
-        padding: 10,
-        backgroundColor: 'rgba(0,0,35,0.5)',
-        borderRadius: 30,
-    },
-    controlBox: {
-        alignItems: 'center',
-        width: 100,
+    tabText: {
+        fontWeight: '400',
+        fontSize: 16,
+        // lineHeight: 30
+        marginTop: 8,
     },
 })
