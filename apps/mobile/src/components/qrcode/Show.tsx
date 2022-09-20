2f543034ca629035a3bff55f713f9cbe04691618
Otto G <5030059+ottodevs@users.noreply.github.com>
2022-09-20 07:55:27 +0200

    feat: create component to show a generated QR

---
+++ apps/mobile/src/components/qrcode/Show.tsx
@@ -0,0 +1,10 @@
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { Text } from '../Themed'
+
+export function Show() {
+    return (
+        <SafeAreaView>
+            <Text>This is just a placeholder</Text>
+        </SafeAreaView>
+    )
+}
