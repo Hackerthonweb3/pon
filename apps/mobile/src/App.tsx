f17f74c521e6ad678d293cfb6baf64268353e942
Otto G <5030059+ottodevs@users.noreply.github.com>
2022-09-21 11:35:36 +0200

    feat: initial orbis-sdk integration

--- apps/mobile/src/App.tsx
+++ apps/mobile/src/App.tsx
@@ -1,9 +1,13 @@
+import '@business-card/orbis-sdk-react-native/utils/polyfills_light_crypto'
+import { Orbis } from '@business-card/orbis-sdk-react-native'
 import { StatusBar } from 'expo-status-bar'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 
 import useCachedResources from './hooks/useCachedResources'
 import useColorScheme from './hooks/useColorScheme'
 import Navigation from './navigation'
+
+const orbis = new Orbis()
 
 export default function App() {
     const isLoadingComplete = useCachedResources()
