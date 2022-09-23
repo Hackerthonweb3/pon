bba5c75c2f35b554517363478465c166d6e9a1ae
Otto G <5030059+ottodevs@users.noreply.github.com>
2022-09-23 20:32:57 +0200

    build: add baber module resolver to alias paths

--- apps/mobile/babel.config.js
+++ apps/mobile/babel.config.js
@@ -2,6 +2,15 @@ module.exports = function (api) {
     api.cache(true)
     return {
         presets: ['babel-preset-expo'],
-        plugins: ['babel-plugin-styled-components', '@babel/plugin-proposal-export-namespace-from'],
+        plugins: [
+            'babel-plugin-styled-components',
+            '@babel/plugin-proposal-export-namespace-from',
+            [
+                'module-resolver',
+                {
+                    alias: { '~/*': ['./src/*'] },
+                },
+            ],
+        ],
     }
 }
