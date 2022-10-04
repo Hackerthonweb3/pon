d63dfb8d480333bf2492b5b89d5f65565de77775
Otto G <5030059+ottodevs@users.noreply.github.com>
2022-10-04 12:20:30 -0500

    feat: test cermic/lit connection flow for mobile

--- apps/web/src/pages/logic.tsx
+++ apps/web/src/pages/logic.tsx
@@ -1,13 +1,78 @@
 import { useContext, useEffect, useState } from 'react'
+// import { useAccount } from 'wagmi'
+import { CustomConnect } from '~/components/CustomConnect'
+import { useOrbis } from '~/hooks'
+import { useCeramicSession } from '~/hooks/useCeramicSession'
+
 import { useAccount, useConnect, useDisconnect, useProvider } from 'wagmi'
 import { Button } from '@chakra-ui/react'
+import { OrbisContext } from '~/contexts'
+
 export default function App() {
+    const connectedProvider = useProvider()
     const { connector: activeConnector, isConnected } = useAccount({
+        async onConnect({ address, connector, isReconnected }) {
+            console.log('connected by rainbow')
+            const provider = await connector?.getProvider()
+            console.log('the provider', provider)
+            console.log('lets connect orbis')
+            // const orbisConnected = await orbis?.connect(provider)
+            // console.log('orbis connected?', orbisConnected)
+            // if (orbisConnected.status === 200) {
+            //     setIsOrbisConnected(true)
+            // }
+        },
+        async onDisconnect() {
+            await orbis?.logout()
+            console.log('orbis disconnected by rainbow')
+            setIsOrbisConnected(false)
+        },
     })
+    const orbis = useContext(OrbisContext)
+    const [isOrbisConnected, setIsOrbisConnected] = useState(false)
+    const [isCeramicConnected, setIsCeramicConnected] = useState(false)
+    const [isLitConnected, setIsLitConnected] = useState(false)
     const { connect, connectors, error, isLoading, pendingConnector } = useConnect({
+        async onSuccess(data, variables, context) {
+            console.log('succesfully connected', { data, variables, context })
+            const provider = await data.connector?.getProvider()
+            console.log('the provider', provider)
+            console.log('lets connect orbis')
+            // const orbisConnected = await orbis?.connect((data.provider as any)['provider'])
+            // console.log('orbis connected?', orbisConnected)
+            // if (orbisConnected.status === 200) {
+            //     setIsOrbisConnected(true)
+            // }
+        },
     })
+
+    const connectOrbis = async () => {
+        const theProvider = await activeConnector?.getProvider()
+        // alert(theProvider)
+        // console.log('the provider', theProvider.send, theProvider.sendAsync, theProvider.request)
+        const result = await orbis?.connect(theProvider)
+        if (result.status === 200) {
+            setIsOrbisConnected(true)
+        }
+    }
+
+    const { disconnect } = useDisconnect({
+        async onSuccess() {
+            await orbis?.logout()
+            console.log('orbis disconnected')
+            setIsOrbisConnected(false)
+        },
+    })
+
+    useEffect(() => {
+        setIsCeramicConnected(Boolean(localStorage.getItem('ceramic-session')))
+        setIsLitConnected(Boolean(localStorage.getItem('lit-auth-signature')))
+    }, [isOrbisConnected])
+
+    // console.log('connectors', connectors)
     return (
         <>
+            <CustomConnect />
             {isConnected ? <div>Connected to {activeConnector?.name}</div> : <div>Disconnected</div>}
             <br />
             <hr />
@@ -31,6 +96,15 @@ export default function App() {
             <hr />
             <br />
             Wallet connected? {`${isConnected}`}
+            <br />
+            Ceramic connected? {`${isCeramicConnected}`}
+            <br />
+            Lit connected? {`${isLitConnected}`}
+            <br />
+            <br />
+            <Button disabled={isOrbisConnected || !isConnected} onClick={connectOrbis}>
+                Connect Orbis
+            </Button>
         </>
     )
 }
