9b7a6fbbf213f266fc0dcf3050a704d21e0c8ed8
Otto G <5030059+ottodevs@users.noreply.github.com>
2022-10-04 12:18:31 -0500

    feat: improve ceramic/lit  session management

--- apps/web/src/hooks/useCeramicSession.ts
+++ apps/web/src/hooks/useCeramicSession.ts
@@ -1,4 +1,5 @@
 import { useRouter } from 'next/router'
+import { useState } from 'react'
 import { useAccount } from 'wagmi'
 import { useOrbis } from './useOrbis'
 
@@ -6,31 +7,69 @@ export const useCeramicSession = () => {
     const { push } = useRouter()
     const { address: connectedAddress } = useAccount()
     const { connect } = useOrbis()
+    const [isCeramicConnected, setIsCeramicConnected] = useState(false)
+    const [isLitConnected, setIsLitConnected] = useState(false)
+
+    const checkCeramicPreviousSession = (address: string) => {
+        return localStorage.getItem(`ceramic-session-${address}`)
+    }
+
+    const checkLitPreviousSession = (address: string) => {
+        return localStorage.getItem(`lit-auth-signature-${address}`)
+    }
 
     const onConnect = async ({ address }: any) => {
         const lowerCaseAddress = address?.toLowerCase()
 
-        const ceramicPreviousSession = localStorage.getItem(`ceramic-session-${lowerCaseAddress}`)
-        if (!ceramicPreviousSession) {
-            console.log('Ceramic session not found, creating a new one')
+        const ceramicCurrentSession = localStorage.getItem('ceramic-session')
+        const litCurrentSession = localStorage.getItem('lit-auth-signature')
+
+        if (ceramicCurrentSession && litCurrentSession) {
+            console.log('Ceramic and Lit re-connected')
+            setIsCeramicConnected(true)
+            setIsLitConnected(true)
+            return
+        }
+
+        const ceramicPreviousSession = checkCeramicPreviousSession(lowerCaseAddress)
+        const litPreviousSession = checkLitPreviousSession(lowerCaseAddress)
+
+        if (!ceramicPreviousSession || !litPreviousSession) {
+            console.log('Ceramic or Lit sessions not found, creating new ones')
             await connect()
         } else {
-            console.log('Ceramic session found, restoring')
-            localStorage.setItem('ceramic-session', ceramicPreviousSession)
+            if (ceramicPreviousSession) {
+                console.log('Ceramic session found, restoring')
+                localStorage.setItem('ceramic-session', ceramicPreviousSession)
+            }
+            if (litPreviousSession) {
+                console.log('Lit session found, restoring')
+                localStorage.setItem('lit-auth-signature-', litPreviousSession)
+            }
         }
+        if (checkCeramicPreviousSession(lowerCaseAddress)) setIsCeramicConnected(true)
+        if (checkLitPreviousSession(lowerCaseAddress)) setIsLitConnected(true)
     }
 
     const onDisconnect = () => {
-        // don't remove this item, rename to the account and reuse the same as lit-auth-signature
+        const lowerCaseAddress = connectedAddress?.toLowerCase()
+
         const ceramicSession = localStorage.getItem('ceramic-session')
         if (ceramicSession) {
-            const lowerCaseAddress = connectedAddress?.toLowerCase()
             localStorage.setItem(`ceramic-session-${lowerCaseAddress}`, ceramicSession)
             localStorage.removeItem('ceramic-session')
             console.log('Ceramic session saved for address', lowerCaseAddress)
         }
-        push('/')
+
+        const litSession = localStorage.getItem('lit-auth-signature')
+        if (litSession) {
+            localStorage.setItem(`lit-auth-signature-${lowerCaseAddress}`, litSession)
+            localStorage.removeItem('lit-auth-signature')
+            console.log('Lit session saved for address', lowerCaseAddress)
+        }
+        // TODO: re-enable this!!!
+        // push('/')
     }
 
-    return { onConnect, onDisconnect }
+    return { isCeramicConnected, isLitConnected, onConnect, onDisconnect }
 }
