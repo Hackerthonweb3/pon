ec6400bd55f42a57b9d788f3309c805d7e2061f4
Otto G <5030059+ottodevs@users.noreply.github.com>
2022-10-05 14:45:36 -0500

    fix: missing types for useWindowSize

--- apps/web/src/hooks/useWindowSize.ts
+++ apps/web/src/hooks/useWindowSize.ts
@@ -3,7 +3,7 @@ import { useState, useEffect } from 'react'
 export function useWindowSize() {
     // Initialize state with undefined width/height so server and client renders match
     // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
-    const [windowSize, setWindowSize] = useState({
+    const [windowSize, setWindowSize] = useState<{ width: any; height: any }>({
         width: undefined,
         height: undefined,
     })
