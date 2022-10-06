c1bd894bf23d70f0720498bac52f2eef4930d2e8
Otto G <5030059+ottodevs@users.noreply.github.com>
2022-10-06 14:41:05 -0500

    feat: add resets and globals for chakra

--- apps/web/src/lib/theme.ts
+++ apps/web/src/lib/theme.ts
@@ -1,3 +1,4 @@
+import 'focus-visible/dist/focus-visible'
 import { extendTheme } from '@chakra-ui/react'
 
 export const theme = extendTheme({
@@ -9,5 +10,16 @@ export const theme = extendTheme({
         heading: `vt323, monospace`,
         body: `vt323, monospace`,
         h4: `vt323, monospace`,
+    },
+    styles: {
+        global: {
+            'html, body': {
+                height: '100%',
+                backgroundColor: '#2B2E38',
+            },
+            ', #__next, .css-0': {
+                height: '100%',
+            },
+        },
     },
 })
