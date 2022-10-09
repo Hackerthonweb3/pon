c0299cef5705e22717a954e06dc7131be0fb6462
lsalda12 <lgomez@rivian.com>
2022-10-09 08:23:28 -0500

    Menu bar adjustments

--- apps/web/src/components/Navbar.tsx
+++ apps/web/src/components/Navbar.tsx
@@ -4,13 +4,15 @@ import Image from 'next/image'
 
 import EventIcon from '~/media/svg/event.svg';
 import ProfileIcon from '~/media/svg/profile.svg';
+import { useRouter } from 'next/router';
 
 export default function Navbar() {
+    const router = useRouter();
     return (
         <nav>
             <ul className={styles.nav}>
-                <li className={styles['nav-item']}><Link href="/event"><Image src={EventIcon} alt="event" /></Link></li>
-                <li className={styles['nav-item']}><Link href="/profile"><Image src={ProfileIcon} alt="event" /></Link></li>
+                <li className={!router.pathname.includes('event') ? styles['nav-item'] : styles['nav-item--active']}><Link href="/event"><Image src={EventIcon} alt="event" /></Link></li>
+                <li className={router.pathname.includes('event') ? styles['nav-item'] : styles['nav-item--active']}><Link href="/profile"><Image src={ProfileIcon} alt="event" /></Link></li>
             </ul>
         </nav>
     )
