ad0e89c3110340fad772f3d7c6065d3ffeeb8795
Otto G <5030059+ottodevs@users.noreply.github.com>
2022-10-11 15:37:42 -0500

    feat: improve QR code looking

---
+++ apps/web/src/components/QrCode.tsx
@@ -0,0 +1,43 @@
+import { Center, Spinner } from '@chakra-ui/react'
+import QRCodeStyling from 'qr-code-styling'
+import { useContext, useEffect, useRef } from 'react'
+import { OrbisContext } from '~/contexts'
+
+const qrCode = new QRCodeStyling({
+    width: 300,
+    height: 300,
+    dotsOptions: {
+        type: 'dots',
+        color: '#bfc0c3',
+    },
+    backgroundOptions: { color: '#2B2E38' },
+    cornersSquareOptions: { type: 'square', color: '#bfc0c3' },
+    cornersDotOptions: { type: 'square', color: '#bfc0c3' },
+    qrOptions: {
+        typeNumber: 5,
+        errorCorrectionLevel: 'L',
+        mode: 'Byte',
+    },
+})
+
+export default function QrCode({ did }: { did?: string }) {
+    const qrElement = useRef<HTMLDivElement>(null)
+
+    useEffect(() => {
+        if (qrElement.current) {
+            qrCode.update({
+                data: `https://web3card.vercel.app/${did}`,
+            })
+            qrCode.append(qrElement.current)
+        }
+    }, [qrElement])
+
+    if (!did)
+        return (
+            <Center h='full'>
+                <Spinner size='xl' />
+            </Center>
+        )
+
+    return <div ref={qrElement} />
+}
